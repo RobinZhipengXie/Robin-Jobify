e946154df3a040a7934b5bd054a54ed00580bcd9
RobinZhipengXie <96404290+RobinZhipengXie@users.noreply.github.com>
2022-03-13 17:14:26 -0700

    front end

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+import Alert from './Alert'
+import FormRow from './FormRow'
+import Logo from './logo'
+export { Logo, FormRow, Alert }
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,31 @@
+import main from '../assets/images/main-robin.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            a Job <span>tracking</span> web
+          </h1>
+          <p>
+            This website helps with tracking jobs during your job hunting.
+            Please feel free to use it and good luck! Created by Robin Xie.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
